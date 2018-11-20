b40730b3ed92613ac06faadf7ce3eae979e2b6a1
hopamohsen245 <44228723+hopamohsen245@users.noreply.github.com>
2018-11-20 19:00:19 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,28 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "p";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on('ready', async() => {
+var server = "514478694157385743"; // ايدي السررفر
+var channel = "514484992961806340";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**plexythebest,plexythebest,plexythebest,plexythebest,plexythebest,plexythebest,plexythebest,plexythebest,plexythebest,plexythebest,plexythebest,plexythebest,plexythebest,plexythebest,plexythebest, **')
+    },305);
+})
+
+client.on('message',function(message) {
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply("**You Dont Have Any Permations:tools: **");
+if(!args) return;
+message.channel.send(`${args}`);
+    message.delete();
+}
+});
+
+ 
+client.login(process.env.BOT_TOKEN);
